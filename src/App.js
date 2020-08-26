b608be9a41ea0157b586c27d91c033e94f9fd9c5
scubaguba345 <brandon.gubitosa@gmail.com>
2020-08-26 11:59:07 -0400

    first two deliverables done

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ class App extends React.Component {
 
   appClickHandler = (bacon) => {
     bacon.favorite = true
-    this.setState({bey: bacon})
-  }
+    //this.setState({bey: bacon})
 
-  render(){
+    let beyArray = this.state.bey
+    beyArray.push(bacon)
+    this.setState({
+      bey: beyArray.filter(bacon => bacon.favorite)})
+    }
+  
+  
+
+  render() {
     return (
       <div className="container">
         <BeyContainer appClickHandler={this.appClickHandler}/>
         <Favorites bey={this.state.bey}/>
       </div>
     );
-  };
+  }
 }
+
 
 
 export default App;
--- src/Favorites.js
+++ src/Favorites.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import BeyCard from './BeyCard.js'
-import beyArray from './api.js'
+//import beyArray from './api.js'
 
 
 class Favorites extends Component {
 
- 
+  renderBeys = () => {
+    return this.props.bey.map(beyonceObj => <BeyCard key={beyonceObj.id} bey={beyonceObj}/>)
+  }
 
   render() {  
-    let beys = beyArray//.map(beyObj => <BeyCard key={beyObj.id} bey={beyObj} />)//appClickHandler = {this.props.appClickHandler}/>)
-    //beys.this.props.favorite === true ? {beys} : null
-    console.dir(beys[1].favorite)
+    // let beys = beyArray//.map(beyObj => <BeyCard key={beyObj.id} bey={beyObj} />)//appClickHandler = {this.props.appClickHandler}/>)
+    // //beys.this.props.favorite === true ? {beys} : null
+    // console.dir(beys[1].favorite)
     return (
       <div className="favorites">
         <h1>Favorites</h1>
-         <h3><BeyCard {beys.favorite === true ? {beys} : null} /></h3> 
+         {this.renderBeys()}
       </div>
     );
   }
