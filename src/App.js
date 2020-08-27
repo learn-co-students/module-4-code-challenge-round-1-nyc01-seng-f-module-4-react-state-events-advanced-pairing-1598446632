86733f6b194cd4aa6fa27abc235169dfbd4a0675
hahm-d <dhahm01@gmail.com>
2020-08-27 09:39:10 -0400

    original solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+
 import React from "react";
 import "./App.css";
+import beyArray from './api'
+import BeyContainer from './BeyContainer'
+import Favorites from './Favorites'
+class App extends React.Component {
 
-const App = () => {
-  return (
-    <div className="container">
-      <BeyContainer />
-      <Favorites />
-    </div>
-  );
+  state = {
+    beyArray: beyArray
+  }
+
+  containerClickHandler = (id) => {
+    // Our App needs a way to know which object was clicked on
+    console.log("ID in App: ", id)
+    // Our array needs to be appropriately updated
+    let newArray = [...this.state.beyArray]
+    let foundObj = newArray.find((beyObj) => beyObj.id === id)
+    foundObj.favorite = true
+    foundObj.num_of_clicks = foundObj.num_of_clicks + 1
+    // console.log(foundObj)
+    // Our app needs to rerender
+    this.setState({ beyArray: newArray })
+  }
+
+  favoriteClickHandler = (id) => {
+    let newArray = [...this.state.beyArray]
+    let foundObj = newArray.find((beyObj) => beyObj.id === id)
+    foundObj.favorite = false
+
+    this.setState({ beyArray: newArray }, () => window.alert("I got a hot sauce in my bag, swag"))
+
+  }
+
+  filteredBeys = () => {
+    return this.state.beyArray.filter((beyObj) => beyObj.favorite)
+  }
+
+  sortedBeys = () => {
+    return this.state.beyArray.sort((beyObjA, beyObjB) => beyObjB.num_of_clicks - beyObjA.num_of_clicks)
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <BeyContainer array={this.sortedBeys()} clickHandler={this.containerClickHandler} />
+        <Favorites array={this.filteredBeys()} clickHandler={this.favoriteClickHandler} />
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/BeyContainer.js
+++ src/BeyContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import BeyCard from './BeyCard'
 class BeyContainer extends React.Component {
+
+  beys = () => {
+    return this.props.array.map((beyObj) => <BeyCard key={beyObj.id} bey={beyObj} clickHandler={this.props.clickHandler} />)
+  }
+
   render() {
     return (
       <div className="index">
         <h1>Index</h1>
-        {/*list of beys*/}
+        {this.beys()}
       </div>
     );
   }
