dd7f44954abbe7d4a58cd4d4da3e1df285c4c8bd
yulia <juliana.ny2008@gmail.com>
2020-08-27 15:04:49 -0400

    2nd deliverable done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ state = {
   beyArray: beyArray,
   gifName: "gif name",
   gifUrl: "gif url",
-  userInput: "Search for name"
+  userInput: ""
 }
 
 containerClickHandler = (id) =>{
@@ -32,20 +32,6 @@ submitHandler = event => {
         this.setState({beyArray: dataArray})      
       }  
       
-searchHandler = event => {
-        event.preventDefault()
-        let input = event.target.value.toUpperCase() 
-        console.log(input)
-        // next steps:
-        // need to set new state "matchesSearch" 
-        // create filter function which: 
-              // copy beyArray
-              // change false to true for those beyObj.name that matches input
-              // update state beyArray => beyContainer will only render cards that matches search input
-      
-
-
-}
 
 filteredBeys = () => {
   return this.state.beyArray.filter((beyObj) => beyObj.favorite===true)
--- src/BeyContainer.js
+++ src/BeyContainer.js
@@ -2,16 +2,20 @@ import React from "react";
 import BeyCard from "./BeyCard"
 
 class BeyContainer extends React.Component {
+  filterBeys = () => {
+    return this.props.array.filter((beyObj) => beyObj.name.toLowerCase().includes(this.props.userInput.toLowerCase()))
+  }
  
   render() {
-    let beys = this.props.array.map((card) => <BeyCard key={card.id} bey={card} clickHandler={this.props.clickHandler}/>)
+    
+    let beys = this.filterBeys().map((card) => <BeyCard key={card.id} bey={card} clickHandler={this.props.clickHandler}/>)
 
     return (
       <div className="index">
         <h1>Index</h1>
         <input type="text"  name = "userInput"
-               onKeyUp={event => this.props.searchHandler(event)} 
                value={this.props.userInput}
+               placeholder = "Search for name"
                onChange = {event => this.props.changeHandler(event)} > 
         </input>
         {beys}
